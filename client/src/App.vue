da10c20f7c26c8298747d723acf454b76a7453aa
ptrickd <patrickdion8@gmail.com>
2024-04-12 20:31:20 -0600

    Cleaning useless code + fews lines

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const brand = 'Toyota'
+const model = 'Rav4'
+const kms = 165000
+</script>
+
+<template>
+  <header></header>
+  <body>
+    <ol>
+      <li>Brand: {{ brand }}</li>
+      <li>Model: {{ model }}</li>
+      <li>Mileage: {{ kms }} km</li>
+    </ol>
+  </body>
+</template>
+
+<style scoped></style>
