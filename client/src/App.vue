1b756b8106199080465fd1dfa23499a89fe70e1a
ptrickd <patrickdion8@gmail.com>
2024-07-09 12:00:25 -0600

    Simple Footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <main>
-    <Toast />
+  <main class="container">
     <NavBar />
+    <Toast />
     <RouterView />
+    <FooterPage class="footer" />
   </main>
 </template>
 
 <style scoped>
-main {
+.container {
   min-width: 400px;
-  max-width: 1000px;
+  max-width: 900px;
+}
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
 }
 </style>
 
@@ -17,4 +24,5 @@ main {
 import Toast from 'primevue/toast'
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+import FooterPage from './components/FooterPage.vue'
 </script>
---
+++ client/src/components/FooterPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <vue-divider class="divider" />
+    <div class="info">
+      <div class="line"><p>2024</p></div>
+      <div class="line"><vue-button class="pi pi-github" text></vue-button></div>
+      <div class="line"><p>GPLv3</p></div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.container {
+  min-height: 50px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.info {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin-left: 15%;
+  margin-right: 15%;
+}
+.line {
+  display: flex;
+  align-items: center;
+}
+</style>
